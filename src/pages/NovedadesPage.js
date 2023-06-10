a670f889b2981b61ac794766b6ccabdb872da02e
Elias Bulesevich <ebulesevich@frba.utn.edu.ar>
2023-06-10 11:06:13 -0300

    Tarea M4U2

---
+++ src/pages/NovedadesPage.js
@@ -0,0 +1,36 @@
+import '../styles/components/pages/NovedadesPage.css';
+
+const NovedadesPage = (props) => {
+    return (
+        <main className="holder">
+        <h2>Novedades</h2>
+        <div className="novedades">
+            <h3>Titulo</h3>
+            <h4>Subtítulo</h4>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo nihil vel dicta qui, rem cum enim
+                blanditiis assumenda ipsam officia recusandae dolorem, asperiores unde. Esse soluta voluptatum numquam
+                dicta libero!
+            </p>
+            <h3>Titulo</h3>
+            <h4>Subtítulo</h4>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo nihil vel dicta qui, rem cum enim
+                blanditiis assumenda ipsam officia recusandae dolorem, asperiores unde. Esse soluta voluptatum numquam
+                dicta libero!
+            </p>
+            
+            <h3>Titulo</h3>
+            <h4>Subtítulo</h4>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo nihil vel dicta qui, rem cum enim
+                blanditiis assumenda ipsam officia recusandae dolorem, asperiores unde. Esse soluta voluptatum numquam
+                dicta libero!
+            </p>
+            
+        </div>
+    </main>
+    );
+}
+
+export default NovedadesPage;
